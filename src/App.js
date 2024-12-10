3b5c471fa544a6d2a1f2f22db248d255dd7998e8
ZYisus <kinichiaudante@gmail.com>
2024-12-10 11:40:03 -0500

    Mensaje descriptivo del cambio

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import React from 'react';
 import { Navbar, Nav, Container, Card, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
 
 // Configuración de Chart.js
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-// Datos y opciones del gráfico
-const chartData = {
-  labels: ['Comidas y Bebidas', 'Compras', 'Viviendas', 'Transporte', 'Gastos financieros'],
-  datasets: [
-    {
-      label: 'Gastos (en unidades)',
-      data: [120, 90, 70, 40, 60],
-      backgroundColor: ['#007bff', '#6610f2', '#6f42c1', '#e83e8c', '#dc3545'],
-    },
-  ],
-};
-
-const chartOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Gastos por Categoría',
-    },
-  },
-};
 
 function App() {
   return (
@@ -52,10 +25,8 @@ function App() {
       </Navbar>
 
       {/* Gráfico de ventas */}
-      <Container className="my-5" id="grafico">
-        <h2 className="mb-4">Gráfico de gastos de este mes</h2>
-        <Bar data={chartData} options={chartOptions} />
-      </Container>
+      
+
 
       {/* Información adicional */}
       <Container className="my-5">
